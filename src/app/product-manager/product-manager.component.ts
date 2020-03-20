2532d227c9b76c2b4b8f88af608b32f7a1239f20
lwhlng <hoanglogg@gmail.com>
2020-03-20 23:05:17 +0700

    1 file modified

--- src/app/product-manager/product-manager.component.ts
+++ src/app/product-manager/product-manager.component.ts
@@ -1,29 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { data } from '../MockData';
-import { Product } from '../Product';
+import { Component, OnInit } from "@angular/core";
+import { data } from "../MockData";
+import { Product } from "../Product";
 
 @Component({
-  selector: 'app-product-manager',
-  templateUrl: './product-manager.component.html',
-  styleUrls: ['./product-manager.component.css']
+  selector: "app-product-manager",
+  templateUrl: "./product-manager.component.html",
+  styleUrls: ["./product-manager.component.css"]
 })
 export class ProductManagerComponent implements OnInit {
-
   products = data;
   selected: Product;
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     console.log(this.selected);
   }
-  detailProduct(product){
+  detailProduct(product) {
     this.selected = product;
     console.log(this.selected);
   }
-  removeItem(id){
+  removeItem(id) {
     this.products = this.products.filter(x => x.id !== id);
     console.log(this.selected.id);
     console.log(id);
-
+    if (id == this.selected.id) {
+      this.selected = null;
+    }
   }
 }
