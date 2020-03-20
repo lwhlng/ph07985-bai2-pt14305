2d37f2d095b3bd214eaf4fc6760d32da36c78167
lwhlng <hoanglogg@gmail.com>
2020-03-20 22:25:09 +0700

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { CategoryComponent } from './category/category.component';
-import { ProductListComponent } from './product-list/product-list.component';
 import { ProductManagerComponent } from './product-manager/product-manager.component';
 import { SliderComponent } from './slider/slider.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, CategoryComponent, ProductListComponent, ProductManagerComponent, SliderComponent ],
+  declarations: [ AppComponent, HelloComponent, CategoryComponent, ProductManagerComponent, SliderComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
